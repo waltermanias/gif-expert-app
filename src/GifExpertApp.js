7c8cda3d140b98eb05e5243fd4490a492224bd24
Walter Manias <wmanias@gmail.com>
2021-10-20 17:43:00 -0300

    Add category component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
+      <hr />
+
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+};
---
+++ src/components/AddCategory.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const AddCategory = ({ setCategories }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      setCategories((c) => [...c, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button>Agregar</button>
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired,
+};
+
+export { AddCategory };
